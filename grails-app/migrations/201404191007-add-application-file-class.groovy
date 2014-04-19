9fcb14aedc243974092615cab4e3b2eea6841c50
Casey Scarborough <caseyscarborough@gmail.com>
2014-04-19 10:24:55 -0400

    Remove duplicate migration data.

--- grails-app/migrations/201404191007-add-application-file-class.groovy
+++ grails-app/migrations/201404191007-add-application-file-class.groovy
@@ -1,33 +1,5 @@
 databaseChangeLog = {
 
-	changeSet(author: "Casey (generated)", id: "1397916457343-1") {
-		createTable(tableName: "application") {
-			column(autoIncrement: "true", name: "id", type: "bigint") {
-				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "applicationPK")
-			}
-
-			column(name: "version", type: "bigint") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "comments", type: "varchar(255)")
-
-			column(name: "date_created", type: "datetime") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "last_updated", type: "datetime") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "reviewed_by_id", type: "bigint")
-
-			column(name: "user_id", type: "bigint") {
-				constraints(nullable: "false")
-			}
-		}
-	}
-
 	changeSet(author: "Casey (generated)", id: "1397916457343-2") {
 		createTable(tableName: "application_file") {
 			column(autoIncrement: "true", name: "id", type: "bigint") {
@@ -52,30 +24,10 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "Casey (generated)", id: "1397916457343-6") {
-		createIndex(indexName: "FK5CA40550E549346F", tableName: "application") {
-			column(name: "reviewed_by_id")
-		}
-	}
-
-	changeSet(author: "Casey (generated)", id: "1397916457343-7") {
-		createIndex(indexName: "FK5CA40550F61654C3", tableName: "application") {
-			column(name: "user_id")
-		}
-	}
-
 	changeSet(author: "Casey (generated)", id: "1397916457343-8") {
 		createIndex(indexName: "FK57B0BEC1BCCB1", tableName: "application_file") {
 			column(name: "application_id")
 		}
-	}
-
-	changeSet(author: "Casey (generated)", id: "1397916457343-3") {
-		addForeignKeyConstraint(baseColumnNames: "reviewed_by_id", baseTableName: "application", constraintName: "FK5CA40550E549346F", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "user", referencesUniqueColumn: "false")
-	}
-
-	changeSet(author: "Casey (generated)", id: "1397916457343-4") {
-		addForeignKeyConstraint(baseColumnNames: "user_id", baseTableName: "application", constraintName: "FK5CA40550F61654C3", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "user", referencesUniqueColumn: "false")
 	}
 
 	changeSet(author: "Casey (generated)", id: "1397916457343-5") {
